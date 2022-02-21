85c146d9b833efedd4f9df3c8089e3840d11b291
robinvoor <r.vanoorschot@mpo-ventures.com>
2022-02-21 17:09:18 +0100

    Include vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '065190c51f5ca951d7620c5ea95a0b3d9f4d1520',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '065190c51f5ca951d7620c5ea95a0b3d9f4d1520',
+            'dev_requirement' => false,
+        ),
+        'erusev/parsedown' => array(
+            'pretty_version' => '1.7.4',
+            'version' => '1.7.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown',
+            'aliases' => array(),
+            'reference' => 'cb17b6477dfff935958ba01325f2e8a2bfa6dab3',
+            'dev_requirement' => false,
+        ),
+        'maneuver/bitbucket-wp-updater' => array(
+            'pretty_version' => 'v0.0.3',
+            'version' => '0.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../maneuver/bitbucket-wp-updater',
+            'aliases' => array(),
+            'reference' => '078401f27ab38422b8ea3f33085d8ee6a6c1f006',
+            'dev_requirement' => false,
+        ),
+    ),
+);
